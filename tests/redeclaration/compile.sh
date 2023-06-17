73f1f297cca235451275d02ee045b3d17f722e70
deneribeiro10 <deneribeiro10@hotmail.com>
2023-06-17 17:57:35 -0300

    add(+): detect redefinition and undefined variables

---
+++ tests/redeclaration/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yacc -d -v example.y                                     # cria y.tab.h y.tab.c
+lex example.l                                                       # cria lex.yy.c
+gcc lex.yy.c y.tab.c ../../src/*.c -o main      # compile
