7284219f86b7468334aa4a9f288d08b9280e6221
JhonatMiranda <jhonata.miranda@ufv.br>
2023-07-03 19:11:10 -0300

    add(+): pseudo in progress

---
+++ src/pseudo/pseudo-context.h
@@ -0,0 +1,34 @@
+#ifndef PSEUDOCONTEXT_H
+#define PSEUDOCONTEXT_H
+
+#include "../symbol-table/symbolTable.h"
+
+typedef struct pseudoContext PseudoContext;
+
+struct pseudoContext
+{
+    SymbolTable* symbolTable;
+    FILE* fileName;
+    //LabelStack *if_else, *if_exit, *rep_entry, *rep_exit, *for_stmt, *for_update;
+};
+
+
+PseudoContext *pseudo_ContextNew(FILE* filename,SymbolTable* st);
+int pseudoCodeGenAssign(PseudoContext *context, char* var, int reg2);
+int pseudoCodeGenInteger(PseudoContext *context, int num);
+int pseudoCodeGenBinaryOperator(PseudoContext *context, int op, int reg1, int reg2);
+int  pseudoCodeGenUnaryOperator(PseudoContext *context, int op, int reg1);
+int pseudoCodeGenVariable(PseudoContext *context,char* var);
+void pseudoSaveRegisters(PseudoContext *context);
+void pseudoCodeExpr(PseudoContext *context, int reg);
+void pseudoCodeElse(PseudoContext *context);
+void pseudoCodeExit(PseudoContext *context);
+void pseudoCodeRepEntry(PseudoContext *context);
+void pseudoCodeRepExpr(PseudoContext *context, int reg);
+void pseudoCodeRepExit(PseudoContext *context);
+void pseudoCodeForStmtUpdate(PseudoContext *context);
+void pseudoCodeForEntryStmt(PseudoContext *context);
+void pseudoCodeForUpdateExit(PseudoContext *context);
+
+
+#endif
